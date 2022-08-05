d5ffe4b580435f55550d0bae7e197051b46fc6e1
pedro930122 <pedro930122@gmail.com>
2022-08-05 04:37:48 -0500

    add: added CheckTx logic

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"github.com/dgraph-io/badger/v3"
 	abcitypes "github.com/tendermint/tendermint/abci/types"
 )
@@ -26,8 +27,39 @@ func (KVStoreApplication) DeliverTx(req abcitypes.RequestDeliverTx) abcitypes.Re
 	return abcitypes.ResponseDeliverTx{Code: 0}
 }
 
-func (KVStoreApplication) CheckTx(req abcitypes.RequestCheckTx) abcitypes.ResponseCheckTx {
-	return abcitypes.ResponseCheckTx{Code: 0}
+func (app *KVStoreApplication) isValid(tx []byte) (code uint32) {
+	parts := bytes.Split(tx, []byte("="))
+	if len(parts) != 2 {
+		return 1
+	}
+	key, value := parts[0], parts[1]
+
+	// check if the same key=value already exists
+	err := app.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(key)
+		if err != nil && err != badger.ErrKeyNotFound {
+			return err
+		}
+		if err == nil {
+			return item.Value(func(val []byte) error {
+				if bytes.Equal(val, value) {
+					code = 2
+				}
+				return nil
+			})
+		}
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	return code
+}
+
+func (app *KVStoreApplication) CheckTx(req abcitypes.RequestCheckTx) abcitypes.ResponseCheckTx {
+	code := app.isValid(req.Tx)
+	return abcitypes.ResponseCheckTx{Code: code, GasWanted: 1}
 }
 
 func (KVStoreApplication) Commit() abcitypes.ResponseCommit {
